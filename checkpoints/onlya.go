7532f8c810bfa84e71e9135023faec7dd4a0d8b6
mudimurtala <ibnmuhyideen95@gmail.com>
2026-04-24 15:26:39 +0100

    Add solution to file

---
+++ checkpoints/onlya.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+// ## onlya
+// ### Instructions
+// Write a program that displays a `a` character on the standard output. (and nothing else)
+
+func main() {
+	z01.PrintRune('a')
+}
---
+++ checkpoints/onlyz.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+// ## onlyz
+// ### Instructions
+// Write a program that displays a `z` character on the standard output. (and nothing else)
+
+func main() {
+	z01.PrintRune('z')
+}
